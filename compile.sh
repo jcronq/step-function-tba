bee4cab86cfe4d2a7c31796dd5d3365a1dae7913
jcronq@gmail.com <jcronq@gmail.com>
2021-01-04 16:11:12 -0700

    latest changes

--- compile.sh
+++ compile.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 declare -A varmap
-varmap["print_console_queue_url"]="https://sqs.us-west-2.amazonaws.com/755547719452/step-function-tba.fifo"
+varmap["print_console_queue_url"]="https:\/\/sqs.us-west-2.amazonaws.com\/755547719452\/step-function-tba.fifo"
 
 mkdir -p build
 cp ./state-machine.json ./build/state-machine.json
+echo "${!varmap[@]}"
 
 for var_name in "${!varmap[@]}"; do
     var="${varmap[$var_name]}"
     echo "$var_name $var"
-    sed -i "s/<%$var_name%>/$var/g" ./build/state-machine.json
+    sed "s/<%$var_name%>/$var/g" ./build/state-machine.json > ./build/state-machine.json-new
+    mv ./build/state-machine.json-new ./build/state-machine.json
 done
